36281c48312a6c7b91ae1928d9b9a25d04539373
DESKTOP-L97VA5M\oshiz <oshizawadai.1192@gmail.com>
2021-09-12 12:28:00 +0900

    add Shader add Editor

---
+++ Editor/CardShaderGUI.cs
@@ -0,0 +1,289 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CardShaderGUI : ShaderGUI
+{
+    MaterialProperty StackColor;
+
+    MaterialProperty ParallaxScale;
+    
+    MaterialProperty Layer0;
+    MaterialProperty Layer0Height;
+    MaterialProperty Layer0StackColor;
+
+    MaterialProperty UseLayer1;
+    MaterialProperty Layer1;
+    MaterialProperty Layer1Height;
+    MaterialProperty Layer1StackColor;
+
+    MaterialProperty UseLayer2;
+    MaterialProperty Layer2;
+    MaterialProperty Layer2Height;
+    MaterialProperty Layer2StackColor;
+
+    MaterialProperty UseLayer3;
+    MaterialProperty Layer3;
+    MaterialProperty Layer3Height;
+    MaterialProperty Layer3StackColor;
+
+    MaterialProperty UseLayer4;
+    MaterialProperty Layer4;
+    MaterialProperty Layer4Height;
+    MaterialProperty Layer4StackColor;
+
+    MaterialProperty BackTex;
+    MaterialProperty BackTexStackColor;
+
+    MaterialProperty DissolveTex;
+    MaterialProperty DissolveAmount;
+
+    MaterialProperty UseEdge;
+    MaterialProperty EdgeColor;
+    MaterialProperty EdgeRange;
+    MaterialProperty EdgeBlur;
+
+    Texture a;
+    
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] Prop)
+    {
+        var material = (Material)materialEditor.target;
+
+        StackColor = FindProperty("_StackColor", Prop, false);
+
+        ParallaxScale = FindProperty("_ParallaxScale", Prop, false);
+
+        Layer0 = FindProperty("_Layer0", Prop, false);
+        Layer0Height = FindProperty("_Layer0Height", Prop, false);
+        Layer0StackColor = FindProperty("_Layer0StackColor", Prop, false);
+
+        UseLayer1 = FindProperty("_UseLayer1", Prop, false);
+        Layer1 = FindProperty("_Layer1", Prop, false);
+        Layer1Height = FindProperty("_Layer1Height", Prop, false);
+        Layer1StackColor = FindProperty("_Layer1StackColor", Prop, false);
+
+        UseLayer2 = FindProperty("_UseLayer2", Prop, false);
+        Layer2 = FindProperty("_Layer2", Prop, false);
+        Layer2Height = FindProperty("_Layer2Height", Prop, false);
+        Layer2StackColor = FindProperty("_Layer2StackColor", Prop, false);
+
+        UseLayer3 = FindProperty("_UseLayer3", Prop, false);
+        Layer3 = FindProperty("_Layer3", Prop, false);
+        Layer3Height = FindProperty("_Layer3Height", Prop, false);
+        Layer3StackColor = FindProperty("_Layer3StackColor", Prop, false);
+
+        UseLayer4 = FindProperty("_UseLayer4", Prop, false);
+        Layer4 = FindProperty("_Layer4", Prop, false);
+        Layer4Height = FindProperty("_Layer4Height", Prop, false);
+        Layer4StackColor = FindProperty("_Layer4StackColor", Prop, false);
+
+        BackTex = FindProperty("_BackTex", Prop, false);
+        BackTexStackColor = FindProperty("_BackTexStackColor", Prop, false);
+
+        DissolveTex = FindProperty("_DissolveTex", Prop, false);
+        DissolveAmount = FindProperty("_DissolveAmount", Prop, false);
+
+        UseEdge = FindProperty("_UseEdge", Prop, false);
+        EdgeColor = FindProperty("_EdgeColor", Prop, false);
+        EdgeRange = FindProperty("_EdgeRange", Prop, false);
+        EdgeBlur = FindProperty("_EdgeBlur", Prop, false);
+
+
+
+        // base.OnGUI (materialEditor, Prop);
+
+        GUILayout.Label("Parallax", EditorStyles.boldLabel);
+        using (new EditorGUILayout.VerticalScope("box"))
+        {
+            using (new EditorGUILayout.HorizontalScope("box"))
+            {
+                GUILayout.Label("ParallaxScale");
+                ParallaxScale.floatValue = EditorGUILayout.Slider(ParallaxScale.floatValue, ParallaxScale.rangeLimits.x, ParallaxScale.rangeLimits.y);
+                GUILayout.Space(64.0f);
+            }
+            // materialEditor.ShaderProperty(ParallaxScale, new GUIContent("ParallaxScale"));
+        }
+
+        // ================================================================================================
+        GUILayout.Box("", GUILayout.Height(2), GUILayout.ExpandWidth(true));
+        // ================================================================================================
+
+        GUILayout.Label("StackColor", EditorStyles.boldLabel);
+        using (new EditorGUILayout.VerticalScope("box"))
+        {   
+            using (new EditorGUILayout.HorizontalScope("box"))
+            {
+                GUILayout.Label("StackColor");
+                StackColor.colorValue = EditorGUILayout.ColorField(StackColor.colorValue);
+                GUILayout.Space(64.0f);
+            }
+            
+            // materialEditor.ShaderProperty(StackColor, new GUIContent("StackColor"));
+        }
+
+        // ================================================================================================
+        GUILayout.Box("", GUILayout.Height(2), GUILayout.ExpandWidth(true));
+        // ================================================================================================
+
+        GUILayout.Label("Texture", EditorStyles.boldLabel);
+        using (new EditorGUILayout.VerticalScope("box"))
+        {
+                GUILayout.Label("Layer0");
+                using (new EditorGUILayout.VerticalScope("box"))
+                {
+                    GUILayout.Space(2.0f);
+                    using (new EditorGUILayout.HorizontalScope())
+                    {
+                        var options = new []{GUILayout.Width (64), GUILayout.Height (64)};
+                        using (new EditorGUILayout.VerticalScope())
+                        {
+                            using (new EditorGUILayout.HorizontalScope())
+                            {
+                                // GUILayout.Space(8.0f);
+                                GUILayout.Label("Layer0 MainTexture");
+                            }
+                            
+                            using (new EditorGUILayout.HorizontalScope())
+                            {
+                                // GUILayout.Space(8.0f);
+                                GUILayout.Label("Height");
+                                Layer0Height.floatValue = EditorGUILayout.Slider(Layer0Height.floatValue, Layer0Height.rangeLimits.x, Layer0Height.rangeLimits.y);
+                            }
+                            
+                            using (new EditorGUILayout.HorizontalScope())
+                            {
+                                // GUILayout.Space(8.0f);
+                                GUILayout.Label("Color");
+                                Layer0StackColor.colorValue = EditorGUILayout.ColorField(Layer0StackColor.colorValue);
+                            }
+                            
+                        }
+                        Layer0.textureValue = EditorGUILayout.ObjectField(Layer0.textureValue, typeof(Texture), false, options) as Texture;
+                    }
+                }
+        }
+
+        LayerTextureLayout(materialEditor, UseLayer1, Layer1, Layer1Height, Layer1StackColor, "Layer1");
+
+        LayerTextureLayout(materialEditor, UseLayer2, Layer2, Layer2Height, Layer2StackColor, "Layer2");
+
+        LayerTextureLayout(materialEditor, UseLayer3, Layer3, Layer3Height, Layer3StackColor, "Layer3");
+
+        LayerTextureLayout(materialEditor, UseLayer4, Layer4, Layer4Height, Layer4StackColor, "Layer4");
+        
+        using (new EditorGUILayout.VerticalScope("box"))
+        {
+            GUILayout.Label("BackTexture");
+            using (new EditorGUILayout.VerticalScope())
+            {
+                GUILayout.Space(2.0f);
+                using (new EditorGUILayout.HorizontalScope())
+                {
+                    var options = new []{GUILayout.Width (64), GUILayout.Height (64)};
+                    using (new EditorGUILayout.HorizontalScope())
+                    {
+                        GUILayout.Label("Color");
+                        BackTexStackColor.colorValue = EditorGUILayout.ColorField(BackTexStackColor.colorValue);
+                    }
+                    BackTex.textureValue = EditorGUILayout.ObjectField(BackTex.textureValue, typeof(Texture), false, options) as Texture;
+                }
+            }
+        }
+
+        // ================================================================================================
+        GUILayout.Box("", GUILayout.Height(2), GUILayout.ExpandWidth(true));
+        // ================================================================================================
+
+        GUILayout.Label("Dissolve", EditorStyles.boldLabel);
+        using (new EditorGUILayout.VerticalScope("box"))
+        {
+            GUILayout.Space(2.0f);
+                using (new EditorGUILayout.HorizontalScope())
+                {
+                    var options = new []{GUILayout.Width (64), GUILayout.Height (64)};
+                    using (new EditorGUILayout.HorizontalScope())
+                    {
+                        GUILayout.Label("DissolveAmount");
+                        DissolveAmount.floatValue = EditorGUILayout.Slider(DissolveAmount.floatValue, DissolveAmount.rangeLimits.x, DissolveAmount.rangeLimits.y);
+                    }
+                    DissolveTex.textureValue = EditorGUILayout.ObjectField(DissolveTex.textureValue, typeof(Texture), false, options) as Texture;
+                }
+        }
+
+        // ================================================================================================
+        GUILayout.Box("", GUILayout.Height(2), GUILayout.ExpandWidth(true));
+        // ================================================================================================
+
+        GUILayout.Label("Edge", EditorStyles.boldLabel);
+        using (new EditorGUILayout.VerticalScope("box"))
+        {
+            materialEditor.ShaderProperty(UseEdge, new GUIContent("UseEdge"));
+            if(UseEdge.floatValue > 0.0f)
+            {
+                using (new EditorGUILayout.VerticalScope("box"))
+                {
+                    using (new EditorGUILayout.HorizontalScope())
+                    {
+                        GUILayout.Label("EdgeColor");
+                        EdgeColor.colorValue = EditorGUILayout.ColorField(EdgeColor.colorValue);
+                        GUILayout.Space(64.0f);
+                    }
+                    
+                    using (new EditorGUILayout.HorizontalScope())
+                    {
+                        GUILayout.Label("EdgeRange");
+                        EdgeRange.floatValue = EditorGUILayout.Slider(EdgeRange.floatValue, EdgeRange.rangeLimits.x, EdgeRange.rangeLimits.y);
+                        GUILayout.Space(64.0f);
+                    }
+                
+                    using (new EditorGUILayout.HorizontalScope())
+                    {
+                        GUILayout.Label("EdgeBlur");
+                        EdgeBlur.floatValue = EditorGUILayout.Slider(EdgeBlur.floatValue, EdgeBlur.rangeLimits.x, EdgeBlur.rangeLimits.y);
+                        GUILayout.Space(64.0f);
+                    }
+                }
+            }
+        }
+    
+    }
+
+    void LayerTextureLayout(MaterialEditor ed,            MaterialProperty UseLayerToggle, MaterialProperty LayerTex, 
+                            MaterialProperty LayerHeight, MaterialProperty LayerCol,       string LayerName)
+    {
+        using (new EditorGUILayout.VerticalScope("box"))
+        {
+            ed.ShaderProperty(UseLayerToggle, new GUIContent("Use" + LayerName));
+            if(UseLayerToggle.floatValue > 0.0f)
+            {
+                using (new EditorGUILayout.VerticalScope("box"))
+                {
+                    GUILayout.Space(2.0f);
+                    using (new EditorGUILayout.HorizontalScope())
+                    {
+                        var options = new []{GUILayout.Width (64), GUILayout.Height (64)};
+                        using (new EditorGUILayout.VerticalScope())
+                        {
+                            GUILayout.Label(LayerName + " MainTexture");
+                            using (new EditorGUILayout.HorizontalScope())
+                            {
+                                GUILayout.Label("Height");
+                                LayerHeight.floatValue = EditorGUILayout.Slider(LayerHeight.floatValue, LayerHeight.rangeLimits.x, LayerHeight.rangeLimits.y);
+                            }
+                            
+                            using (new EditorGUILayout.HorizontalScope())
+                            {
+                                GUILayout.Label("Color");
+                                LayerCol.colorValue = EditorGUILayout.ColorField(LayerCol.colorValue);
+                            }
+                        }
+                        LayerTex.textureValue = EditorGUILayout.ObjectField(LayerTex.textureValue, typeof(Texture), false, options) as Texture;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
---
+++ a3ParallaxCardShader.shader
@@ -0,0 +1,240 @@
+﻿Shader "a3ParallaxCardShader/Parallax"
+{
+    Properties
+    {
+        _StackColor("StackColor", color) = (1.0, 1.0, 1.0 ,1.0)
+
+        _ParallaxScale("Parallax Scale", Range(0.0, 1.0)) = 1.0
+
+        _Layer0 ("Layer0", 2D) = "white" {}
+        _Layer0Height("Layer0Hegiht", Range(0.0, 1.0)) = 1.0
+        _Layer0StackColor("Layer0StackColor", color) = (1.0, 1.0, 1.0, 1.0)
+
+        [Toggle(_USE_LAYER_ONE)] _UseLayer1("UseLayer1", Float) = 0
+        _Layer1("Layer1", 2D) = "white" {}
+        _Layer1Height("Layer1Hegiht", Range(0.0, 1.0)) = 1.0
+        _Layer1StackColor("Layer1StackColor", color) = (1.0, 1.0, 1.0, 1.0)
+
+        [Toggle(_USE_LAYER_TWO)] _UseLayer2("UseLayer2", Float) = 0
+        _Layer2("Layer2", 2D) = "white" {}
+        _Layer2Height("Layer2Hegiht", Range(0.0, 1.0)) = 1.0
+        _Layer2StackColor("Layer2StackColor", color) = (1.0, 1.0, 1.0, 1.0)
+
+        [Toggle(_USE_LAYER_THREE)] _UseLayer3("UseLayer3", Float) = 0
+        _Layer3("Layer3", 2D) = "white" {}
+        _Layer3Height("Layer3Hegiht", Range(0.0, 1.0)) = 1.0
+        _Layer3StackColor("Layer3StackColor", color) = (1.0, 1.0, 1.0, 1.0)
+
+        [Toggle(_USE_LAYER_FOUR)] _UseLayer4("UseLayer4", Float) = 0
+        _Layer4("Layer4", 2D) = "white" {}
+        _Layer4Height("Layer4Hegiht", Range(0.0, 1.0)) = 1.0
+        _Layer4StackColor("Layer4StackColor", color) = (1.0, 1.0, 1.0, 1.0)
+
+        _BackTex("BackTexture", 2D) = "white" {}
+        _BackTexStackColor("BackTexStackColor", color) = (1.0, 1.0, 1.0, 1.0)
+
+        _DissolveTex("DissolveTexture", 2D) = "white" {}
+        _DissolveAmount("DissolveAmount", Range(0.0, 1.0)) = 0.0
+
+        [Toggle(_USE_EDGE)] _UseEdge("UseEdge", Float) = 0
+        [HDR]_EdgeColor("EdgeColor", color) = (1.0, 1.0, 1.0,1.0)
+        _EdgeRange("EdgeRange", Range(0.0, 0.5)) = 0.1
+        _EdgeBlur("EdgeBlur", Range(0.001, 0.3)) = 0.1
+
+    }
+    SubShader
+    {
+        Tags 
+        { 
+            "RenderType"="Transparent"
+        }
+        Cull Off
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #pragma shader_feature _USE_LAYER_ONE
+            #pragma shader_feature _USE_LAYER_TWO
+            #pragma shader_feature _USE_LAYER_THREE
+            #pragma shader_feature _USE_LAYER_FOUR
+            #pragma shader_feature _USE_EDGE
+
+            #pragma target 4.0
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+                float3 normal : NORMAL;
+                float4 tangent : TANGENT;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+
+                //=====================================================
+                float3 viewDir : TEXCOORD1;
+                float3 normal : NORMAL;
+            };
+
+            sampler2D _Layer0;
+            float _Layer0Height;
+            float4 _Layer0StackColor;
+
+            #ifdef _USE_LAYER_ONE
+                sampler2D _Layer1;
+                float _Layer1Height;
+                float4 _Layer1StackColor;
+            #endif
+
+            #ifdef _USE_LAYER_TWO
+                sampler2D _Layer2;
+                float _Layer2Height;
+                float4 _Layer2StackColor;
+            #endif
+
+            #ifdef _USE_LAYER_THREE
+                sampler2D _Layer3;
+                float _Layer3Height;
+                float4 _Layer3StackColor;
+            #endif
+
+            #ifdef _USE_LAYER_FOUR
+                sampler2D _Layer4;
+                float _Layer4Height;
+                float4 _Layer4StackColor;
+            #endif
+
+            sampler2D _BackTex;
+            float4 _BackTexStackColor;
+
+            float _ParallaxScale;
+
+            float3 _StackColor;
+
+            sampler2D _DissolveTex;
+            float _DissolveAmount;
+
+            #if _USE_EDGE
+                float3 _EdgeColor;
+                float _EdgeRange;
+                float _EdgeBlur;
+            #endif
+
+            float2 Parallax(float height, float parallaxScale, float3 viewDir)
+            {
+                return (height - 1.0) * (viewDir.xy / viewDir.z) * parallaxScale;
+            }
+
+            float2 getUV(float3 viewDir, float layerHeight, float parallaxScale, float2 uv)
+            {
+                viewDir = normalize(viewDir);
+                float2 offset = Parallax(layerHeight, parallaxScale, viewDir);
+                uv += offset;
+                return uv;
+            }
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+
+                TANGENT_SPACE_ROTATION;
+                o.viewDir = mul(rotation, ObjSpaceViewDir(v.vertex));
+                return o;
+            }
+
+            float4 frag (v2f i, float facing : VFACE) : SV_Target
+            {
+                // UV計算
+                float2 uv0 = getUV(i.viewDir, _Layer0Height, _ParallaxScale, i.uv);
+                
+                #ifdef _USE_LAYER_ONE
+                    float2 uv1 = getUV(i.viewDir, _Layer1Height, _ParallaxScale, i.uv);
+                #endif
+
+                #ifdef _USE_LAYER_TWO
+                    float2 uv2 = getUV(i.viewDir, _Layer2Height, _ParallaxScale, i.uv);
+                #endif
+
+                #ifdef _USE_LAYER_THREE
+                    float2 uv3 = getUV(i.viewDir, _Layer3Height, _ParallaxScale, i.uv);
+                #endif
+
+                #ifdef _USE_LAYER_FOUR
+                    float2 uv4 = getUV(i.viewDir, _Layer4Height, _ParallaxScale, i.uv);
+                #endif
+
+
+                // ディゾルブ計算
+                float dissolve = tex2D(_DissolveTex, i.uv).r;
+                dissolve *= 0.999;
+
+                float isVisible = dissolve - _DissolveAmount;
+                clip(isVisible);
+
+                // Edge計算
+                #if _USE_EDGE
+                    float edge = smoothstep(_EdgeRange + _EdgeBlur, _EdgeRange, isVisible);
+                    float4 isEdge = float4(edge * _EdgeColor.r, edge * _EdgeColor.g, edge * _EdgeColor.b, edge);
+                #endif
+
+                // tex2D計算
+                float4 layer0, layer1, layer2, layer3, layer4, backTex;
+                layer0 = layer1 = layer2 = layer3 = layer4 = backTex = 0.0;
+
+                layer0.a *= 0.0;
+
+                layer0 += tex2D(_Layer0, uv0) * _Layer0StackColor;
+
+                #ifdef _USE_LAYER_ONE
+                    layer1 += tex2D(_Layer1, uv1) * _Layer1StackColor;
+                #endif
+
+                #ifdef _USE_LAYER_TWO
+                    layer2 += tex2D(_Layer2, uv2) * _Layer2StackColor;
+                #endif
+
+                #ifdef _USE_LAYER_THREE
+                    layer3 += tex2D(_Layer3, uv3) * _Layer3StackColor;
+                #endif
+
+                #ifdef _USE_LAYER_FOUR
+                    layer4 += tex2D(_Layer4, uv4) * _Layer4StackColor;
+                #endif
+
+                backTex += tex2D(_BackTex, i.uv) * _BackTexStackColor;
+                
+                // 色計算
+                float3 color = layer0;
+                color = color * (1-layer1.a) + layer1 * layer1.a;
+                color = color * (1-layer2.a) + layer2 * layer2.a;
+                color = color * (1-layer3.a) + layer3 * layer3.a;
+                color = color * (1-layer4.a) + layer4 * layer4.a;
+                
+                color = color * _StackColor;
+
+                float3 backColor = backTex.rgb;
+                backColor = backColor * _StackColor;
+
+                #if _USE_EDGE
+                    color = color * (1.0 - isEdge.a) + isEdge * isEdge.a;
+                    backColor = backColor * (1.0 - isEdge.a) + isEdge * isEdge.a;
+                # endif
+
+                return (facing > 0 ? float4(color, 0.0) : float4(backColor, 1.0));
+            }
+            ENDCG
+        }
+    }
+    CustomEditor "CardShaderGUI"
+}
